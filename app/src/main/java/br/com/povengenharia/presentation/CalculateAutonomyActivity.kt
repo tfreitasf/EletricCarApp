08cd8446c1625c2f3ac270baed300483109f338f
tfreitasf <tfreitasf@gmail.com>
2023-08-25 14:02:39 -0300

    refactoring the mainactivity objects to CalculateAutonomyActivity

--- app/src/main/java/br/com/povengenharia/presentation/CalculateAutonomyActivity.kt
+++ app/src/main/java/br/com/povengenharia/presentation/CalculateAutonomyActivity.kt
@@ -1,13 +1,52 @@
 package br.com.povengenharia.presentation
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.povengenharia.R
 
 class CalculateAutonomyActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, ) {
+
+    lateinit var price: EditText
+    lateinit var travelledDistance: EditText
+    lateinit var btnCalculate: Button
+    lateinit var result: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculate_autonomy)
+
+        setupView()
+        setupListners()
+    }
+
+    fun setupView() {
+        price = findViewById(R.id.et_kwhprice)
+        btnCalculate = findViewById(R.id.bt_calculate)
+        travelledDistance = findViewById(R.id.et_travelleddistance)
+        result = findViewById(R.id.tv_result)
+
+
+    }
+
+    fun setupListners() {
+
+        btnCalculate.setOnClickListener {
+            calculate()
+        }
+
+    }
+
+    fun calculate() {
+        val price = price.text.toString().toFloat()
+        val travelledDistance = travelledDistance.text.toString().toFloat()
+
+        val costKmTraveled = price / travelledDistance
+        result.text = costKmTraveled.toString()
+
     }
 }
+
+
--- app/src/main/java/br/com/povengenharia/presentation/MainActivity.kt
+++ app/src/main/java/br/com/povengenharia/presentation/MainActivity.kt
@@ -1,52 +1,27 @@
 package br.com.povengenharia.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import br.com.povengenharia.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var price: EditText
-    lateinit var travelledDistance: EditText
-    lateinit var btnCalculate: Button
-    lateinit var result: TextView
-    lateinit var fabtnCalculate : FloatingActionButton
+
+    lateinit var fabtnCalculate: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupView()
-        setupListners()
 
-    }
-
-    fun setupView() {
-        price = findViewById(R.id.et_kwhprice)
-        btnCalculate = findViewById(R.id.bt_calculate)
-        travelledDistance = findViewById(R.id.et_travelleddistance)
-        result = findViewById(R.id.tv_result)
         fabtnCalculate = findViewById(R.id.fab_calculate)
+        fabtnCalculate.setOnClickListener {
+            startActivity(Intent(this, CalculateAutonomyActivity::class.java))
 
-    }
-
-    fun setupListners() {
-
-        btnCalculate.setOnClickListener {
-            calculate()
         }
     }
-
-    fun calculate() {
-        val price = price.text.toString().toFloat()
-        val travelledDistance = travelledDistance.text.toString().toFloat()
-
-        val costKmTraveled = price / travelledDistance
-        result.text = costKmTraveled.toString()
-
-    }
 }
+
+
