2de3009c207b5f561ec55cf8043599ed224b399c
tfreitasf <tfreitasf@gmail.com>
2023-10-16 22:41:51 -0300

    feat: Adicionado Retrofit para busca de dados da API
    - Configurado o Retrofit para fazer solicitações à API de carros.
    - Adicionada a função para buscar dados de carros da API.
    - Atualizada a função onResume para buscar carros somente quando há conexão com a internet.

--- app/src/main/java/br/com/povengenharia/domain/Car.kt
+++ app/src/main/java/br/com/povengenharia/domain/Car.kt
@@ -1,11 +1,13 @@
 package br.com.povengenharia.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Car (
-    val id: Int,
-    val modelName : String,
-    val price : String,
-    val battery : String,
-    val horsePower : String,
-    val recharge : String,
-    val urlPhoto: String
+    @SerializedName("id") val id: Int,
+    @SerializedName("modelName") val modelName : String,
+    @SerializedName("preco") val price : String,
+    @SerializedName("bateria") val battery : String,
+    @SerializedName("potencia") val horsePower : String,
+    @SerializedName("recarga") val recharge : String,
+    @SerializedName("urlPhoto") val urlPhoto: String
 )
